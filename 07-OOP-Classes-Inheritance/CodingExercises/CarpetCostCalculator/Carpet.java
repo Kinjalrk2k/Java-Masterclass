c7a31042850cfd9744659a7d749b77afd5d34735
Kinjal Raykarmakar <kinjalrk2k@gmail.com>
2026-05-12 14:19:08 +0530

    feat: ccoding exercises

---
+++ 07-OOP-Classes-Inheritance/CodingExercises/CarpetCostCalculator/Carpet.java
@@ -0,0 +1,13 @@
+public class Carpet {
+    // write code here
+    private double cost;
+    
+    public Carpet(double cost) {
+        if(cost < 0) cost = 0;
+        this.cost = cost;
+    }
+    
+    public double getCost() {
+        return this.cost;
+    }
+}
---
+++ 07-OOP-Classes-Inheritance/CodingExercises/ComplexNumber.java
@@ -0,0 +1,37 @@
+public class ComplexNumber {
+    // write code here
+    
+    private double real;
+    private double imaginary;
+    
+    public ComplexNumber(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+    
+    public double getReal() {
+        return this.real;
+    }
+    
+    public double getImaginary() {
+        return this.imaginary;
+    }
+    
+    public void add(double real, double imaginary) {
+        this.real += real;
+        this.imaginary += imaginary;
+    }
+    
+    public void add(ComplexNumber c) {
+        this.add(c.real, c.imaginary);
+    }
+    
+    public void subtract(double real, double imaginary) {
+        this.real -= real;
+        this.imaginary -= imaginary;
+    }
+    
+    public void subtract(ComplexNumber c) {
+        this.subtract(c.real, c.imaginary);
+    }
+}
